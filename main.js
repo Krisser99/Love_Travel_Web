ab555472a80137ae85f477fd9fd1d36129671ea8
Krisser99 <93767655+Krisser99@users.noreply.github.com>
2022-03-04 11:16:40 +0700

    change envanto mobile

--- main.js
+++ main.js
@@ -35,10 +35,6 @@
     const lines = $$('.contain__book-line-change');
 
 
-
-
-    
-
 // Auto Change Img
 
     var num = 0;
@@ -68,40 +64,45 @@
         addClassActive();
     }
     
-    ;(function handleChangeImg() {
-        let flag = false
-        // Click Event
-        lines.forEach((line, index) => {
-            const image = images[index];
-            line.onclick = function() {
-                flag = true
-                if(flag) {
-                    $('.contain__book-line-change.active').classList.remove('active');
-                    $('.contain__book-img.active').classList.remove('active');
+    var flag = false
 
-                    line.classList.add('active');
-                    image.classList.add('active');
-                }
-            }
-
-        })
-
-        if(!flag) {
-            setInterval(autoChangeImg, 15000)
-        }
-    })()
+    const autoSwitchImg = setInterval(() => {
+        if(!flag)
+        autoChangeImg()
+    }, 8000)
+        
     
+    
+    // Click Event
+    lines.forEach((line, index) => {
+        const image = images[index];
+        line.onclick = function(e) {
+            $('.contain__book-line-change.active').classList.remove('active');
+            $('.contain__book-img.active').classList.remove('active');
+            line.classList.add('active');
+            image.classList.add('active');
+            flag = true   
+        }
 
+    })
+
+    const containBook = $('.contain__book-right')
+
+    containBook.onmouseleave = function() {
+        setTimeout(() => {
+            flag = false
+        }, 2000);
+    }
 
 // Slider Scroll
 
-const sliderImg = $('.slider__background-img');
+    const sliderImg = $('.slider__background-img');
 
-document.onscroll = function() {
-    const scrollTop = Math.floor(window.scrollY || document.documentElement.scrollTop);
+    document.onscroll = function() {
+        const scrollTop = Math.floor(window.scrollY || document.documentElement.scrollTop);
 
-    const newSliderWidth = (-37 + (scrollTop*5)/100) + '%';
+        const newSliderWidth = (-37 + (scrollTop*5)/100) + '%';
 
-    sliderImg.style.top =  newSliderWidth < 0 + '%' ? newSliderWidth : 0 + '%';
+        sliderImg.style.top =  newSliderWidth < 0 + '%' ? newSliderWidth : 0 + '%';
 
-}
+    }
